dffb4d69ff27a9ec0e33b617ce2058801717f3e7
Frederic Schönberger <24734883+fschoenberger@users.noreply.github.com>
2023-12-04 13:12:56 +0100

    Added optimization to Day 4

--- 04/main.py
+++ 04/main.py
@@ -1,10 +1,13 @@
 import os
-import re
+import time
 
 
 def main():
     with open(os.path.join(os.path.dirname(__file__), "./input.txt")) as f:
-        cards = [(index, *line.split(":")[1].strip().split("|")) for index, line in enumerate(f.readlines())]
+        cards = [
+            (index, *line.split(":")[1].strip().split("|"))
+            for index, line in enumerate(f.readlines())
+        ]
 
         total_wins = 0
 
@@ -30,8 +33,40 @@ def main():
 
         print("\n\n\n=======================")
 
-        tally = 0
+        cache = {}
 
+        def get_num_winning_cards(index: int) -> int:
+            if index in cache:
+                return cache[index]
+
+            winning_numbers, actual_numbers = cards[index][1:]
+            winning_numbers_set = set([int(n.strip()) for n in winning_numbers.split()])
+            actual_numbers_set = set([int(n.strip()) for n in actual_numbers.split()])
+
+            matching_numbers = len(winning_numbers_set.intersection(actual_numbers_set))
+
+            if matching_numbers == 0:
+                cache[index] = 0
+                return 0
+
+            ret = matching_numbers
+            for i in range(index + 1, index + matching_numbers + 1):
+                print(f"Checking card {i}")
+                ret += get_num_winning_cards(i)
+
+            cache[index] = ret
+            return ret
+
+        start_time_block1 = time.perf_counter()
+        sum = len(cards)
+        for i in range(len(cards)):
+            sum += get_num_winning_cards(i)
+
+        end_time_block1 = time.perf_counter()
+        print(f"Part 2 (with memoization): {sum} ({end_time_block1 - start_time_block1} seconds)")
+
+        start_time_block2 = time.perf_counter()
+        tally = 0
         for index, winning_numbers, actual_numbers in cards_queue:
             winning_numbers_set = set([int(n.strip()) for n in winning_numbers.split()])
             actual_numbers_set = set([int(n.strip()) for n in actual_numbers.split()])
@@ -40,17 +75,13 @@ def main():
 
             tally += 1
             if matching_numbers > 0:
-                # if matching_numbers == 1:
-                #     print(f"Card {index + 1}: Adding {matching_numbers} card to the queue (card {index + 2}). Remaining: {len(cards_queue)}")
-                # else:
-                #     print(f"Card {index + 1}: Adding {matching_numbers} cards to the queue (card {index + 2} to {index + matching_numbers + 1}). Remaining: {len(cards_queue)}")
+                cards_queue.extend(cards[index + 1 : index + matching_numbers + 1])
 
-                cards_queue.extend(cards[index + 1: index + matching_numbers + 1])
-
-            if tally % 100000 == 0:
+            if False and tally % 100000 == 0:
                 print(f"Remaining: {len(cards_queue) - tally}")
+        end_time_block2 = time.perf_counter()
 
-        print(f"Part 2: {len(cards_queue)}")
+        print(f"Part 2 (naive implementation): {len(cards_queue)}  ({end_time_block2 - start_time_block2} seconds)")
 
 
 if __name__ == "__main__":
